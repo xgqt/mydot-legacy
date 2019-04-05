3b612756b9c36e07dc3b268e22be61079cde41f7
gnugnat <xgqt@protonmail.com>
2019-04-05 20:58:50 +0200

    remove melpa stable

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -25,7 +25,6 @@
 (setq package-archives '(("ELPA"  . "http://tromey.com/elpa/")
 			 ("gnu"   . "http://elpa.gnu.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")
-			 ("melpa-stable" . "http://stable.melpa.org/packages/")
 			 ("org"   . "https://orgmode.org/elpa/")))
 (package-initialize)
 
